8aadd003145f05ae9e817f465840fb3f2934d4eb
YuyaSakashita <syutayasaka3184@gmail.com>
2024-09-12 14:47:14 +0900

    Initial Commit

---
+++ RealmKakeibo/ShoppingItem.swift
@@ -0,0 +1,15 @@
+//
+//  ShoppingItem.swift
+//  RealmKakeibo
+//
+//  Created by 坂下雄哉 on 2024/09/11.
+//
+
+import Foundation
+import RealmSwift
+
+class ShoppingItem: Object {
+    @Persisted var title: String = ""
+    @Persisted var price: Int = 0
+    @Persisted var isMarked: Bool = false
+}
